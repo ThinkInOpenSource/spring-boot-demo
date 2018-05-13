3a1d9c1995285f914bcf19f88f0f716f2211cd0b
luoxn28 <luoxn28@163.com>
2018-05-13 10:10:49 +0800

    SpringBoot init（aliyun maven仓库、能够运行起来的最小依赖）

---
+++ src/main/java/com/luo/demo/Application.java
@@ -0,0 +1,16 @@
+package com.luo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiangnan
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
